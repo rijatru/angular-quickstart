26dee51d125b97b2177f915fe7b59fc9dbdc5dd4
rijatru <rijatru@gmail.com>
2017-03-18 01:12:57 -0500

    First Commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import {Hero} from "./hero";
+
+@Component({
+
+    selector: 'my-hero-detail',
+    templateUrl: 'hero_detail.html'
+})
+
+export class HeroDetailComponent {
+
+    @Input()
+    hero: Hero;
+}
